da75165dcf30670a5708f26fb1064ba8908caa84
Andy Stead <asteady85@gmail.com>
2022-01-07 10:16:21 +0000

    Extra home page content and added react icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../Hero'
 import HomeLink from '../modules/home-link'
+import { IoCarSportSharp } from 'react-icons/io5'
 
 const Home = () => {
   return (
@@ -8,7 +9,8 @@ const Home = () => {
       <Hero />
       <div className='content tall'>
         <div className='content--wrapper'>
-          <h2 className='text-center'>Our Services</h2>
+          <h2><span>Our Services</span></h2>
+          <p>We offer a wide range of services to suit your individual needs whether it be Outright Purchase, Part Exchanging or just selling your current vehicle. All our services provide you with the best customer service at the most competitive prices.</p>
           <div className='grid grid-gap grid--col3'>
             <HomeLink title='Contract Purchase'>
               <p>This is the description I need to</p>
@@ -31,8 +33,43 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className='content feature text-center'>
-        <p>We offer a wide range of services to suit your individual needs whether it be Outright Purchase, Part Exchanging or just selling your current vehicle. All our services provide you with the best customer service at the most competitive prices.</p>
+      <div className='content feature'>
+        <div className='content--wrapper'>
+          <h2><span>Our Promise To You</span></h2>
+          <p>We offer a wide range of services to suit your individual needs whether it be Outright Purchase, Part Exchanging or just selling your current vehicle. All our services provide you with the best customer service at the most competitive prices.</p>
+          <div className='grid grid-gap grid--col3'>
+            <div className='col'>
+              <div className='round-icon'>
+                <span className='round-icon--wrapper'>
+                  <IoCarSportSharp />
+                </span>
+                <div className='round-icon--content'>
+                  Competitive prices
+                </div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='round-icon'>
+                <span className='round-icon--wrapper'>
+                  <IoCarSportSharp />
+                </span>
+                <div className='round-icon--content'>
+                  Friendly and Helpful
+                </div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='round-icon'>
+                <span className='round-icon--wrapper'>
+                  <IoCarSportSharp />
+                </span>
+                <div className='round-icon--content'>
+                  Great range of cars
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='content tall text-center'>
         <h2>About</h2>
